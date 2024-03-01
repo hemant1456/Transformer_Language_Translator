a7d07c869928967b83eb31bf31e61b3fd5df4672
hemant <hkbhambhu@gmail.com>
2024-03-02 05:03:23 +0530

    added the loss

--- transformer_model.py
+++ transformer_model.py
@@ -6,11 +6,13 @@ from multiheadattention import MultiHeadAttentionBlock
 from model_component import FeedForwardBlock, ProjectionLayer
 from decoder import Decoder, DecoderBlock
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 config = get_config()
 
 class Transformer(LightningModule):
-    def __init__(self, src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project):
+    def __init__(self, src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project, pad_token):
         super().__init__()
         self.src_embed= src_embed
         self.tgt_embed= tgt_embed
@@ -19,11 +21,16 @@ class Transformer(LightningModule):
         self.encoder = encoder
         self.decoder= decoder
         self.project= project
+        self.pad_token = pad_token
     def training_step(self, batch, batch_idx):
         enc_inputs, dec_inputs, src_mask, tgt_mask, labels, src_texts, tgt_texts = batch 
         encoder_output = self.encode(x, src_mask)
         x = self.decode(dec_inputs, encoder_output, src_mask, tgt_mask)
         x = self.project(x)
+        loss = F.cross_entropy(x, labels, ignore_index= self.pad_token)
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr= config['lr'])
+        return optimizer
     def encode(self,x, src_mask):
         x = self.src_embed(x)
         x = self.tgt_pos(x)
@@ -39,6 +46,8 @@ class Transformer(LightningModule):
         return x
     
 def build_transformer(tokenizer_src, tokenizer_tgt):
+    pad_token = tokenizer_tgt.token_to_id("[PAD]")
+    print(pad_token)
     src_embed = InputEmbedding(config["d_model"], tokenizer_src.get_vocab_size())
     tgt_embed = InputEmbedding(config["d_model"], tokenizer_tgt.get_vocab_size())
     src_pos = PositionalEmbedding(config["d_model"], config["seq_len"])
@@ -60,5 +69,5 @@ def build_transformer(tokenizer_src, tokenizer_tgt):
     decoder = Decoder(nn.ModuleList(decoder_blocks))
 
     project = ProjectionLayer(config["d_model"], tokenizer_tgt.get_vocab_size())
-    transformer = Transformer(src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project)
+    transformer = Transformer(src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project,pad_token)
     return transformer
