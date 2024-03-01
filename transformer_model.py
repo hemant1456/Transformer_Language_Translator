ca899d2f94936135801489e689ca48b06ab67cdf
hemant <hkbhambhu@gmail.com>
2024-03-02 04:40:04 +0530

    added build_transformer function

--- transformer_model.py
+++ transformer_model.py
@@ -1,7 +1,17 @@
 from lightning import LightningModule
+from embedding import InputEmbedding, PositionalEmbedding
+from configuration import get_config
+from encoder import Encoder, EncoderBlock
+from multiheadattention import MultiHeadAttentionBlock
+from model_component import FeedForwardBlock, ProjectionLayer
+from decoder import Decoder, DecoderBlock
+import torch.nn as nn
+
+config = get_config()
 
 class Transformer(LightningModule):
     def __init__(self, src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project):
+        super().__init__()
         self.src_embed= src_embed
         self.tgt_embed= tgt_embed
         self.src_pos = src_pos
@@ -27,3 +37,28 @@ class Transformer(LightningModule):
     def decode(self,x):
         x = self.project(x)
         return x
+    
+def build_transformer(tokenizer_src, tokenizer_tgt):
+    src_embed = InputEmbedding(config["d_model"], tokenizer_src.get_vocab_size())
+    tgt_embed = InputEmbedding(config["d_model"], tokenizer_tgt.get_vocab_size())
+    src_pos = PositionalEmbedding(config["d_model"], config["seq_len"])
+    tgt_pos = PositionalEmbedding(config["d_model"], config["seq_len"])
+
+    encoder_blocks = []
+    for _ in range(config["num_encoder_blocks"]):
+        multi_head_block = MultiHeadAttentionBlock(config["d_model"], config["heads"], config["dropout"])
+        feed_forward_block = FeedForwardBlock(config["d_model"], config["d_ff"], config["dropout"])
+        encoder_blocks.append(EncoderBlock(multi_head_block,feed_forward_block))
+    decoder_blocks = []
+    for _ in range(config["num_decoder_blocks"]):
+        self_attention_block = MultiHeadAttentionBlock(config["d_model"], config["heads"], config["dropout"])
+        cross_attention_block = MultiHeadAttentionBlock(config["d_model"], config["heads"], config["dropout"])
+        feed_forward_block = FeedForwardBlock(config["d_model"], config["d_ff"], config["dropout"])
+        decoder_blocks.append(DecoderBlock(self_attention_block, cross_attention_block, feed_forward_block))
+    
+    encoder = Encoder(nn.ModuleList(encoder_blocks))
+    decoder = Decoder(nn.ModuleList(decoder_blocks))
+
+    project = ProjectionLayer(config["d_model"], tokenizer_tgt.get_vocab_size())
+    transformer = Transformer(src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project)
+    return transformer
