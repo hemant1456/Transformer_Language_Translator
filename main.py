ee12f62eb0c143d153a92b0640cbe6ee220965d1
hemant <hkbhambhu@gmail.com>
2024-03-02 08:51:14 +0530

    nicer prining limited val batches

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ if __name__ == "__main__":
     config = get_config()
     os.makedirs(config["model_weights_directory"],exist_ok=True)
     with open("training_logs.txt",'w') as f:
-        f.write("Starting Model Training")
+        f.write("-----Starting Model Training----- \n")
         for key, value in config.items():
             print(f"{key:25}{str(value):15}")
             f.write(f"{key:25}{str(value):15}"+"\n")
+        f.write("\n\n\n")
     
     tokenizer_src, tokenizer_tgt = get_tokenizers()
     train_loader, val_loader = get_dataloaders(tokenizer_src, tokenizer_tgt)
@@ -27,5 +28,5 @@ if __name__ == "__main__":
     model = build_transformer(tokenizer_src, tokenizer_tgt)
 
     trainer = L.Trainer(max_epochs=config["num_epochs"], accelerator=config["accelerator"], devices=config["devices"],
-                        callbacks=[ModelCheckpoint(config["model_weights_directory"])])
+                        callbacks=[ModelCheckpoint(config["model_weights_directory"])],limit_val_batches=10)
     trainer.fit(model, train_loader, val_loader)
