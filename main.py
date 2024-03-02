8b37b7a3f35be2e31bf692459b5c7afd5d41a5e3
hemant <hkbhambhu@gmail.com>
2024-03-02 06:01:52 +0530

    added the main file content to run our model

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from dataloader import get_dataloaders
-train_loader, val_loader = get_dataloaders()
+from tokenizer import get_tokenizers
+import lightning as L
+from configuration import get_config
+from transformer_model import build_transformer
 
+
+if __name__ == "__main__":
+    config = get_config()
+    tokenizer_src, tokenizer_tgt = get_tokenizers()
+    train_loader, val_loader = get_dataloaders(tokenizer_src, tokenizer_tgt)
+
+
+
+    model = build_transformer(tokenizer_src, tokenizer_tgt)
+
+    trainer = L.Trainer(max_epochs=config["num_epochs"], accelerator=config["accelerator"], devices=config["devices"])
+    trainer.fit(model, train_loader, val_loader)
