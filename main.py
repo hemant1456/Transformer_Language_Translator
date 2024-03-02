688be72ae871bae84f5bc4264bb0a3ad73340898
hemant <hkbhambhu@gmail.com>
2024-03-02 07:44:07 +0530

    added training log to external file

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from tokenizer import get_tokenizers
 import lightning as L
 from configuration import get_config
 from transformer_model import build_transformer
+from lightning.pytorch.callbacks import ModelCheckpoint
+import os
 
 
 if __name__ == "__main__":
+    
     config = get_config()
+    os.makedirs(config["model_weights_directory"],exist_ok=True)
+    with open("train_log.txt",'w') as f:
+        f.write("Starting Model Training")
+        for key, value in config.items():
+            print(f"{key:15}{str(value):15}")
+            f.write(f"{key:15}{str(value):15}"+"\n")
+    
     tokenizer_src, tokenizer_tgt = get_tokenizers()
     train_loader, val_loader = get_dataloaders(tokenizer_src, tokenizer_tgt)
 
@@ -14,5 +24,6 @@ if __name__ == "__main__":
 
     model = build_transformer(tokenizer_src, tokenizer_tgt)
 
-    trainer = L.Trainer(max_epochs=config["num_epochs"], accelerator=config["accelerator"], devices=config["devices"])
+    trainer = L.Trainer(max_epochs=config["num_epochs"], accelerator=config["accelerator"], devices=config["devices"],
+                        callbacks=[ModelCheckpoint(config["model_weights_directory"])])
     trainer.fit(model, train_loader, val_loader)
--- transformer_model.py
+++ transformer_model.py
@@ -8,11 +8,14 @@ from decoder import Decoder, DecoderBlock
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from utils import greedy_decode
+import torch
+import random
 
 config = get_config()
 
 class Transformer(LightningModule):
-    def __init__(self, src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project, pad_token,tgt_vocab_size):
+    def __init__(self, src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project,tokenizer_tgt):
         super().__init__()
         self.src_embed= src_embed
         self.tgt_embed= tgt_embed
@@ -21,8 +24,9 @@ class Transformer(LightningModule):
         self.encoder = encoder
         self.decoder= decoder
         self.projection= project
-        self.pad_token = pad_token
-        self.tgt_vocab_size = tgt_vocab_size
+        self.pad_token = tokenizer_tgt.token_to_id("[PAD]")
+        self.tokenizer_tgt = tokenizer_tgt
+        self.tgt_vocab_size = tokenizer_tgt.get_vocab_size()
     def training_step(self, batch, batch_idx):
         enc_inputs, dec_inputs, src_mask, tgt_mask, labels, src_texts, tgt_texts = batch 
         encoder_output = self.encode(enc_inputs, src_mask)
@@ -47,10 +51,31 @@ class Transformer(LightningModule):
     def project(self,x):
         x = self.projection(x)
         return x
+    def on_train_epoch_start(self):
+        self.src_texts = []
+        self.tgt_texts = []
+        self.predicted_texts =[]
+    def validation_step(self,batch, batch_idx):
+        enc_inputs, dec_inputs, src_mask, tgt_mask, labels, src_texts, tgt_texts = batch
+        self.src_texts.append(src_texts[0])
+        self.tgt_texts.append(tgt_texts[0])
+        predicted_text = greedy_decode(self, self.tokenizer_tgt, enc_inputs, src_mask)
+        self.predicted_texts.append(predicted_text)
+    def on_validation_epoch_end(self):
+        for _ in range(2):
+            idx = random.randint(0,len(self.src_texts))
+            with open("train_logs","a") as f:
+                f.write(f"SOURCE: {self.src_texts[idx]}"+"\n")
+                f.write(f"TARGET: {self.tgt_texts[idx]}"+"\n")
+                f.write(f"PREDICTED {self.predicted_texts[idx]}"+"\n\n\n")
+    def on_train_epoch_start(self):
+        with open("train_logs","a") as f:
+                f.write(f"The current learning rate is : {self.trainer.optimizers[0].param_groups[0]['lr']}"+"\n")
+    def on_train_epoch_end(self):
+        with open("train_logs","a") as f:
+                f.write(f"Current Epoch is : {self.current_epoch} and the loss is {self.trainer.callback_metrics['train_loss']}"+"\n")
     
 def build_transformer(tokenizer_src, tokenizer_tgt):
-    pad_token = tokenizer_tgt.token_to_id("[PAD]")
-    print(pad_token)
     src_embed = InputEmbedding(config["d_model"], tokenizer_src.get_vocab_size())
     tgt_embed = InputEmbedding(config["d_model"], tokenizer_tgt.get_vocab_size())
     src_pos = PositionalEmbedding(config["d_model"], config["seq_len"])
@@ -70,8 +95,7 @@ def build_transformer(tokenizer_src, tokenizer_tgt):
     
     encoder = Encoder(nn.ModuleList(encoder_blocks))
     decoder = Decoder(nn.ModuleList(decoder_blocks))
-    tgt_vocab_size = tokenizer_tgt.get_vocab_size()
 
     project = ProjectionLayer(config["d_model"], tokenizer_tgt.get_vocab_size())
-    transformer = Transformer(src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project,pad_token,tgt_vocab_size)
+    transformer = Transformer(src_embed, tgt_embed, src_pos, tgt_pos, encoder, decoder, project,tokenizer_tgt)
     return transformer
