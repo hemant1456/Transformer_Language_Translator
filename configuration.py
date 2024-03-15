53b08f56699b1bb45eba929aa57cb321b560bcee
hemant <hkbhambhu@gmail.com>
2024-03-16 04:04:08 +0530

    adjust some hyperparameter values

--- configuration.py
+++ configuration.py
@@ -7,17 +7,17 @@ def get_config():
     it also defines source and target language for our dataset
     '''
     return {
-        "min_frequency":2, # minimum frequency for tokenzier to include in vocab
+        "min_frequency":1, # minimum frequency for tokenzier to include in vocab
         "batch_size": 32, 
         "num_epochs": 20,
-        "lr": 1e-4,
+        "base_lr": 1e-4, #base learning rate our one cycle epoch, max_lr will be 10* base_lr in our case
         "heads": 8, #number of heads
         "seq_len": 165,
         "d_model": 512,
-        "d_ff": 248, #dimension_feed_forward
+        "d_ff": 512, #dimension_feed_forward
         "dropout": 0.1,
-        "num_encoder_blocks":6,
-        "num_decoder_blocks":6,
+        "num_encoder_blocks":8,
+        "num_decoder_blocks":8,
         "accelerator": "cuda" if torch.cuda.is_available() else "cpu",
         "devices": 1,
         "lang_src": 'en',
